a5a673cf3145de2f0c4e505943aa6aa0afd37832
trib2308337 <trib2308337@student.ctu.edu.vn>
2026-02-18 22:01:40 +0700

    4 bao lì xì

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, session
 import random
 
 app = Flask(__name__)
+app.secret_key = "supersecretkey"   # BẮT BUỘC để dùng session
 
 @app.route("/")
 def index():
-    prizes = [10000, 20000]  # chỉ 2 bao
+    # Tạo mới mỗi lần reload trang
+    prizes = [20000, 20000, 20000, 50000]
     random.shuffle(prizes)
-    app.config["CURRENT_PRIZES"] = prizes
+
+    # Lưu vào session
+    session["CURRENT_PRIZES"] = prizes
+
     return render_template("index.html")
 
 @app.route("/open/<int:index>")
 def open_lixi(index):
-    prizes = app.config.get("CURRENT_PRIZES", [20000, 50000])
-    return jsonify({"prize": prizes[index]})
+    prizes = session.get("CURRENT_PRIZES", [20000, 20000, 20000, 50000])
+
+    # Kiểm tra index hợp lệ
+    if 0 <= index < len(prizes):
+        return jsonify({"prize": prizes[index]})
+    else:
+        return jsonify({"error": "Invalid index"}), 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
